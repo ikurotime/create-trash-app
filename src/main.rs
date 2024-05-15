8fdbd1d14d2e10312df9275b486dde3802754904
David Huertas <davidhuertasortiz@gmail.com>
2024-05-15 04:30:43 +0200

    chore: Update create-trash-app version to 0.0.7

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     // add check for -v or --version
     if args.len() == 2 && (args[1] == "-v" || args[1] == "--version") {
-        println!("create-trash-app v0.0.5");
+        println!("create-trash-app v0.0.7");
         std::process::exit(0);
     }
     if args.len() < 2 {
