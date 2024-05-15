cee7db6614c701eb8c1513aae013b77f7789d9ae
David Huertas <davidhuertasortiz@gmail.com>
2024-05-15 04:22:07 +0200

    chore: Update create-trash-app version to 0.0.5 and add repository link

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::env;
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::{Path};
 use std::error::Error;
 
 fn main() {
@@ -10,12 +10,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     println!("args: {:?}", args);
-    if args.len() != 2 {
+    if args.len() < 2 {
         eprintln!("Usage: create-trash-app <project-name>");
         std::process::exit(1);
     }
 
-    let project_name = &args[2];
+    let project_name = &args[args.len() - 1];
 
     // Create a new project using the template
     if let Err(err) = create_template_app(project_name) {
@@ -42,18 +42,25 @@ fn copy_template(template_path: &str, destination_path: &str) -> Result<(), Box<
     let destination_dir = Path::new(destination_path);
 
     // Create the new project directory
+    if !destination_dir.exists(){
     fs::create_dir(destination_dir)?;
-
-    // Recursively copy template files to the new project directory
+    }
+    // Recursively iterate the directories and copy template files to the new project directory
     for entry in fs::read_dir(template_dir)? {
         let entry = entry?;
         let entry_path = entry.path();
-        let dest_path = destination_dir.join(entry_path.file_name().unwrap());
+        let entry_name = entry.file_name();
+
+        let destination_path = destination_dir.join(entry_name);
 
         if entry_path.is_dir() {
-            fs::create_dir(&dest_path)?;
+            //check if the directory exists
+            if !destination_path.exists() {
+            fs::create_dir(&destination_path)?;
+            }
+            copy_template(&entry_path.to_string_lossy(), &destination_path.to_string_lossy())?;
         } else {
-            fs::copy(&entry_path, &dest_path)?;
+            fs::copy(&entry_path, &destination_path)?;
         }
     }
 
